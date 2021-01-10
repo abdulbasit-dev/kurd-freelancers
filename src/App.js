1610e22a4248eeed8bada17ec1fd178168840a8d
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-01-10 11:56:39 +0300

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="">
       <h1 className="text-red-300">Hello world</h1>
+      <Footer />
     </div>
   );
 }
