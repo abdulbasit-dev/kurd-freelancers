eaaad814984d90339bb8151c7e14db02baad2fd8
mardy muayad <mardypalander@gmail.com>
2021-01-12 10:02:55 +0300

    remove unuse Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Page404 from "./pages/Page404";
