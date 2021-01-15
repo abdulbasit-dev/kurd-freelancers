2ecdf70b4fc05c8e8e368c28d187db43ed6f70c5
Revan Sarbast Salih <revansarbast8@gmail.com>
2021-01-15 21:00:42 +0300

    sign in page done (front-end)

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
-import Button from '../components/Button'
 import FacebookLogo from "./../assets/img/facebook.svg"
 import GoogleLogo from "./../assets/img/google.svg"
 import LinkedInLogo from "./../assets/img/linkedin.svg"
 import GithubLogo from "./../assets/img/github.svg"
-import TextField from '@material-ui/core/TextField';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 const SignIn = () => {
     return (
-        <div className="flex h-screen w-full justify-center items-center -mt-24">
-            <div className="authantiction relative min-h-100 flex px-0 container  flex-col border md:flex-row justify-center items-center ">
-                <div className="flex absolute left-0 bg-lightblue w-full min-h-full md:w-5/12 flex-col pt-5 px-12 text-white">
+        <div className="flex h-screen w-full justify-center items-center -mt-24 ">
+            <div className="authantiction relative min-h-100 flex px-0 rounded container  flex-col md:flex-row justify-center items-center shadow-xl">
+                <div className="flex absolute left-0 bg-lightblue w-full rounded-l min-h-full md:w-5/12 flex-col pt-5 px-12 text-white">
                     <h1 className="text-3xl text-center">What are you</h1>
                     <h1 className="text-3xl text-center ">waiting for?</h1>
                     <p className="text-center mt-12">
                         Register now to get your dream job or best freelancing to make what you want!     
                     </p>
-                    <Button text="Sign up"/>
+                    <div className="flex justify-center mt-20">
+                        <Button variant="contained" color="primary">
+                            Sign up
+                        </Button>
+                    </div>
+                    
 
                 </div>
-                <div className="flex flex-col items-center w-full md:w-7/12 right-0 text-lightblue border-2 h-full absolute ">
+                <div className="flex flex-col items-center w-full md:w-7/12  rounded-r right-0 text-lightblue h-full absolute shadow-xl ">
                     <h1 className="text-4xl mt-2">Log in</h1>
                     <div className="border w-96 my-4"></div>
                     <div className="flex justify-around items-center w-96 ">
@@ -64,10 +69,10 @@ const SignIn = () => {
                             </label>
                             
                         </div>
-                        <div className="flex justify-end">
-                            <button className="border-lightblue border rounded px-2 w-20 h-10 ">
-                                    Log in
-                            </button>
+                        <div className="flex mt-8">
+                            <Button variant="outlined" color="primary">
+                                Log in
+                            </Button>
                         </div>
                     </form>
                 </div>
