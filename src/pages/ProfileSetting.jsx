6115aff693758a6f5b378d1d53af4edab4ad447e
Revan Sarbast Salih <revansarbast8@gmail.com>
2021-02-10 16:06:42 +0300

    profile setting Fixes #24

---
+++ src/pages/ProfileSetting.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProfileSetting = () => {
+    return (
+        <div className="h-screen flex flex-col w-10/12 border-2">
+            <div classname="">
+                
+            </div>
+        </div>
+    )
+}
+
+export default ProfileSetting
