fa43af09af69796f50683801effe1632ec108176
cyb3r <omidbarzani99@gmail.com>
2021-02-10 17:03:32 +0300

    create cover

--- src/pages/ProfileSetting.jsx
+++ src/pages/ProfileSetting.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 const ProfileSetting = () => {
     return (
         <div className="h-screen flex flex-col w-10/12 border-2 mx-auto">
-            <div classname="">
+           <div className="relative w-full h-1/5">
 
+<img  className="absolute w-full h-full " src="https://i.pinimg.com/originals/c1/5c/ba/c15cbae66a8a930a1cb292aaf60bb815.jpg" alt="cover"/>
+<button className="absolute z-20 right-0 mr-8 mt-3 border-2 border-white text-white rounded-md px-4 py-2" >change Cover</button>
+
+
+            
             </div>
-            <div className="">
+            <div className="  ">
 
             </div>
         </div>
