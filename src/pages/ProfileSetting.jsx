4b1619ab3386d8bcbf81cfac0f5826c4449a3dc1
Revan Sarbast Salih <revansarbast8@gmail.com>
2021-02-13 00:45:01 +0300

    profile setting Fixes #24

--- src/pages/ProfileSetting.jsx
+++ src/pages/ProfileSetting.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
 
 const ProfileSetting = () => {
+    const data = {
+        name: "jone",
+        image: "",
+        projects: [
+            {
+                name: "Project Name",
+                des:
+                    " Lorem ipsum dolor sit amet consectetu adipisicing elit. Nisi, error, repellendus tenetur rem sequi illo atque ea corrupti numquam",
+            },
+            {
+                name: "Project Name",
+                des:
+                    " Lorem ipsum dolor sit amet consectetu adipisicing elit. Nisi, error, repellendus tenetur rem sequi illo atque ea corrupti numquam",
+            },
+            {
+                name: "Project Name",
+                des:
+                    " Lorem ipsum dolor sit amet consectetu adipisicing elit. Nisi, error, repellendus tenetur rem sequi illo atque ea corrupti numquam",
+            },
+        ],
+        
+    }
     return (
-        <div className="h-screen flex flex-col w-10/12 border-2 mx-auto">
-            <div classname="">
+        <div className="h-screen flex items-center flex-col w-10/12 border-2 mx-auto">
+             <div className="border w-full h-1/5 bg-black">
 
             </div>
-            <div className="">
+            <div className="border-2 w-9/12 h-3/4 flex -m-10 z-10 bg-white flex-col xl:flex-row">
+                <div className = "w-1/4 border flex flex-col">
 
+                </div>
+                <div className = "w-3/4 border flex flex-col">
+
+                </div>
             </div>
         </div>
     )
