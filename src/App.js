8e0bfb841674408d4037d320f316ea684a57a3dc
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-02-18 00:39:47 +0300

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,65 @@
-import React from "react";
-import Footer from "./components/Footer";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import Page404 from "./pages/Page404/Page404";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn"
-import ProfileSetting from "./pages/ProfileSetting";
-import Singnup from "./pages/Singnup";
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import Page404 from './pages/Page404/Page404';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import ProfileSetting from './pages/ProfileSetting';
+import Singnup from './pages/Singnup';
+import PostJob from './pages/PostJob';
+import Jobs from './pages/Jobs';
 
 function App() {
   return (
     <Router>
-      <div className=" flex flex-col h-screen">
-        <div className="flex-1">
-        <Switch>
-          <Route path="/" exact>
-            <Header />
-            <Home />
-          </Route>
-          <Route path="/about" exact>
-            <Header />
+      <div className=' flex flex-col h-screen'>
+        <div className='flex-1'>
+          <Switch>
+            <Route path='/' exact>
+              <Header />
+              <Home />
+            </Route>
+            <Route path='/post-job' exact>
+              <Header />
+              <PostJob />
+            </Route>
+            <Route path='/jobs' exact>
+              <Header />
+              <Jobs />
+            </Route>
+            <Route path='/about' exact>
+              <Header />
               <About />
               <Footer />
             </Route>
-            <Route path="/profilesetting" exact>
+            <Route path='/profilesetting' exact>
               <Header />
               <ProfileSetting />
-          </Route>
-          <Route path="/signin">
-            <Header />
-              <SignIn />  
-            <Footer />  
-          </Route>
-          <Route path="/signup">
-            <Header />
-            <Singnup/>
-          </Route>
-          <Route path="/profile/:userId">
-            <Header />
+            </Route>
+            <Route path='/signin'>
+              <Header />
+              <SignIn />
+              <Footer />
+            </Route>
+            <Route path='/signup'>
+              <Header />
+              <Singnup />
+            </Route>
+            <Route path='/profile/:userId'>
+              <Header />
               <Profile />
               <Footer />
-          </Route>
-          <Route>
+            </Route>
+            <Route>
               <Page404 />
               <Footer />
-          </Route>
+            </Route>
           </Switch>
-          
         </div>
-        
       </div>
     </Router>
   );
---
+++ src/pages/Jobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Jobs() {
+  return (
+    <div>
+      <h1 className='text-3xl'>Jobs Page</h1>
+    </div>
+  );
+}
+
+export default Jobs;
